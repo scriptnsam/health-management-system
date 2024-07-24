c6f3f4d6187cd350f9b3aea8a2215adb5c00aff8
ScriptnSam <oluwafemisam40@gmail.com>
2024-07-24 03:50:03 +0100

    Added sign in

--- src/controllers/auth/signInController.js
+++ src/controllers/auth/signInController.js
@@ -1,5 +1,16 @@
+const joi = require('joi');
+const { Error, Success } = require('../../utils/response');
+
 const signInController = (req, res) => {
-  return res.status(200).json({ message: "Sign In Controller" })
+  const schema = joi.object({
+    patientNo: joi.number().required().min(0o1).max(999999),
+    password: joi.string().required(),
+  });
+
+  const { error } = schema.validate(req.body);
+  if (error) return Error(res, 400, error.details[0].message);
+
+  return Success(res, 200, 'Sign in successful');
 }
 
 module.exports = { signIn: signInController }
