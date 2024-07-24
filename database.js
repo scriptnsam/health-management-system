4548e2c05e94bbab7ac423ac4d74461e2ec03413
ScriptnSam <oluwafemisam40@gmail.com>
2024-07-24 04:51:57 +0100

    Changed database to postgres

--- database.js
+++ database.js
@@ -4,10 +4,19 @@ const dbName = process.env.DB_NAME;
 const user = process.env.DB_USER;
 const password = process.env.DB_PASSWORD;
 const host = process.env.DB_HOST;
+const port = process.env.DB_PORT;
 
 const sequelize = new Sequelize(dbName, user, password, {
   host,
-  dialect: 'mysql'
+  port: port,
+  dialect: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
 
 module.exports = sequelize;
