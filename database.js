edfcfe52a1a24aa18563edf3e2853f109c617fb8
ScriptnSam <oluwafemisam40@gmail.com>
2024-08-27 22:04:10 +0100

    Edited the database

--- database.js
+++ database.js
@@ -1,14 +1,9 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
-const dbName = process.env.DB_NAME;
-const user = process.env.DB_USER;
-const password = process.env.DB_PASSWORD;
-const host = process.env.DB_HOST;
-const port = process.env.DB_PORT;
 
-const sequelize = new Sequelize(dbName, user, password, {
-  host,
-  port: port,
+const databaseUrl = process.env.DATABASE_URL;
+
+const sequelize = new Sequelize(databaseUrl, {
   dialect: 'postgres',
   logging: false,
   dialectOptions: {
