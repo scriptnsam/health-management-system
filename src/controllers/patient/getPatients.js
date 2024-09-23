ac6f68d4cea7d21f3ba020148bd7eeec995e7291
ScriptnSam <oluwafemisam40@gmail.com>
2024-09-23 04:17:02 +0100

    imported response modules

--- src/controllers/patient/getPatients.js
+++ src/controllers/patient/getPatients.js
@@ -1,11 +1,12 @@
 const { Patient } = require("../../models/Patient");
+const { Success, Error } = require("../../utils/response");
 
 const getPatientDetails = async (req, res) => {
   const { patientId } = req.patient;
   try {
     const patient = await Patient.findOne({ where: { patientId } });
-    if (!patient) return Error(res, 404, "Patient found");
-    return Success(res, 200, "Patient's record retrieved successfully", { medicalProfile: patient });
+    if (!patient) return Error(res, 200, "Patient not found");
+    return Success(res, 200, "Patient's record retrieved successfully", { patient });
   } catch (error) {
     return Error(res, 500, error.message);
   }
