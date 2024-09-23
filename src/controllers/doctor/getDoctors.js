1e75e9160c5d67e0307abc41436b8d1b34ec78ed
ScriptnSam <oluwafemisam40@gmail.com>
2024-09-23 03:34:58 +0100

    added controller for doctor's profile

--- src/controllers/doctor/getDoctors.js
+++ src/controllers/doctor/getDoctors.js
@@ -11,4 +11,14 @@ const getDoctors = async (req, res) => {
   }
 };
 
-module.exports = getDoctors;
+const getDoctorsById = async (req, res) => {
+  // get doctors from database
+  try {
+    const doctor = await Doctor.findOne({ where: { doctorId: req.doctor.id } })
+    return Success(res, 200, 'Doctor Fetched Successfully', { doctor })
+  } catch (error) {
+    return Error(res, 500, error.message)
+  }
+};
+
+module.exports = { getDoctors, getDoctorsById };
--- src/routes/doctor/doctor.js
+++ src/routes/doctor/doctor.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 
-const getDoctors = require('../../controllers/doctor/getDoctors');
+const { getDoctors, getDoctorsById } = require('../../controllers/doctor/getDoctors');
 const { viewAppointments, viewAppointmentsHistory, cancelAppointment, completeAppointment } = require('../../controllers/doctor/appointments');
 const doctorMiddleware = require('../../middlewares/doctorMiddleware');
 
 router.get('/', getDoctors);
+router.get('/getById', doctorMiddleware, getDoctorsById)
 router.get('/appointments', doctorMiddleware, viewAppointments);
 router.get('/appointmentHistory', doctorMiddleware, viewAppointmentsHistory);
 router.put('/appointments/:id/cancel', doctorMiddleware, cancelAppointment);
